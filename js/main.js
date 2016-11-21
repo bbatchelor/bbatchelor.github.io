2e65fbd0187a3ca246c49095a4f902db8df12c1f
sevanti-wal <sevanti@westagilelabs.com>
2016-11-21 17:53:55 +0530

    Header, Footer and initial docs

--- js/main.js
+++ js/main.js
@@ -8,8 +8,18 @@ $(document).ready(function() {
 		$('.site-user-options').toggle();
 		 e.stopPropagation();
 	});
+//OPEN POPUP
+	$('#add_new_employee_link').click(function(e) {
+		$('#add_new_employee').show();
+		e.stopPropagation();
+	});
+	$('.popup-content').click(function(e) {
+		e.stopPropagation();
+	});
+	
 //	CLOSE USER DROPDOWN WHEN CLICKED OUTSIDE
 	$(document).click(function(){
 	    $(".site-user-options").hide();
+	    $('#add_new_employee').hide();
 	});
 });
